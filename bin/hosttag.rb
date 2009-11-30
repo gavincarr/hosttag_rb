ed9e148276f112a78e8855702f8d19107af0a392
Gavin Carr <gavin@openfusion.com.au>
2009-11-30 15:01:43 +0000

    Finish off initial hosttag.rb implementation.

--- bin/hosttag.rb
+++ bin/hosttag.rb
@@ -21,7 +21,7 @@ require 'rubygems'
 require 'redis'
 require 'optparse'
 
-options = { :mode => 'host' }
+options = { :arg_type => 'tag', :all => 0, :all_key => 'all_hosts' }
 
 opts = OptionParser.new
 opts.banner = "Usage: hosttag.rb [options] <tag> [<tag2>...]"
@@ -36,14 +36,14 @@ opts.on('-o', '--or',  'Report hosts with ANY of the given tags (OR result sets)
   options[:rel] = 'or'
 end
 opts.on('-t', '--tag', '--tags', "Tag mode: report tags for the given hosts") do
-  options[:mode] = 'tag'
+  options[:arg_type] = 'host'
 end
 opts.on('-A', '--all', 'Report all hosts') do
-  options[:all] = true
+  options[:all] += 1
 end
 opts.on('-T', '--all-tags', 'Report all tags') do
-  options[:all] = true 
-  options[:mode] = 'tag'
+  options[:all] += 1
+  options[:all_key] = 'all_tags'
 end
 opts.on('-v', '--verbose', 'Give verbose diagnostics') do
   options[:verbose] = true
@@ -53,13 +53,46 @@ def usage(opts)
   exit
 end
 
+# Parse options
 begin
   args = opts.parse(ARGV) 
 rescue => e
   puts "Error: " << e
   usage(opts)
 end
-if args.length == 0
+if args.length == 0 and options[:all] == 0
   usage(opts) 
+end
+
+# Create redis object
+r = Redis.new()
+
+# Standard request
+if options[:all] == 0
+  # Default a rel if we have multiple args
+  if args.length > 1
+    options[:rel] ||= options[:arg_type] == 'tag' ? 'and' : 'or'
+    puts "+ rel: #{options[:rel]}" if options[:verbose]
+  end
+
+  # Map keys to fetch
+  keys = args.collect {|v| "hosttag/#{options[:arg_type]}/#{v}" }
+  puts "+ keys: #{keys.join(' ')}" if options[:verbose]
+
+  # Fetch and report
+  if args.length == 1
+    puts r.set_members( keys ).sort.join(' ')
+  elsif options[:rel] == 'and'
+    puts r.set_intersect( keys ).sort.join(' ')
+  else
+    puts r.set_union( keys ).sort.join(' ')
+  end
+
+# All request
+else
+  key = "hosttag/#{options[:all_key]}"
+  key += "_noskip" if options[:all] == 1
+  puts "+ key: #{key}" if options[:verbose]
+  puts r.set_members(key).sort.join(' ');
 end
 
