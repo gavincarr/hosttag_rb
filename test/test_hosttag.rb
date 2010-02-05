cf020317674ab01b06ca7c0b54d24a4e2c84403e
Gavin Carr <gavin@openfusion.com.au>
2010-02-05 17:42:30 +0000

    Add --namespace argument to binaries; get unit tests working again using own ns.

--- test/test_hosttag.rb
+++ test/test_hosttag.rb
@@ -1,42 +1,50 @@
 #!/usr/bin/ruby
 
 require 'test/unit'
+require 'ftools'
 
 class TestHostTag < Test::Unit::TestCase
 
   TESTS = [
-    [ 'centos',                 'axe mattock nox' ],
-    [ 'laptop',                 'nox' ],
-    [ 'vps',                    'mattock' ],
+    [ 'centos',                 'a m n' ],
+    [ 'laptop',                 'n' ],
+    [ 'vps',                    'm' ],
     [ 'laptop vps',             '' ],
     [ '-a laptop vps',          '' ],
-    [ '-o laptop vps',          'mattock nox' ],
-    [ '-t nox',                 'centos centos5 centos5-i386 laptop' ],
-    [ '-t axe',                 'centos centos5 centos5-x86_64 public' ],
-    [ '-t mattock',             'centos centos4 centos4-x86_64 public vps' ],
-    [ '-t axe nox',             'centos centos5 centos5-i386 centos5-x86_64 laptop public' ],
-    [ '-to axe nox',            'centos centos5 centos5-i386 centos5-x86_64 laptop public' ],
-    [ '-t -o axe nox',          'centos centos5 centos5-i386 centos5-x86_64 laptop public' ],
-    [ '-ta axe nox',            'centos centos5' ],
-    [ '-t -a axe nox',          'centos centos5' ],
-    [ '-t -a axe nox mattock',  'centos' ],
+    [ '-o laptop vps',          'm n' ],
+    [ '-t n',                   'centos centos5 centos5-i386 laptop' ],
+    [ '-t a',                   'centos centos5 centos5-x86_64 public' ],
+    [ '-t m',                   'centos centos4 centos4-x86_64 public vps' ],
+    [ '-t a n',                 'centos centos5 centos5-i386 centos5-x86_64 laptop public' ],
+    [ '-to a n',                'centos centos5 centos5-i386 centos5-x86_64 laptop public' ],
+    [ '-t -o a n',              'centos centos5 centos5-i386 centos5-x86_64 laptop public' ],
+    [ '-ta a n',                'centos centos5' ],
+    [ '-t -a a n',              'centos centos5' ],
+    [ '-t -a a n m',            'centos' ],
     # All cases
-    [ '-A',                     'axe mattock nox' ],
-    [ '-A -A',                  'axe granite hammer mattock nox' ],
+    [ '-A',                     'a m n' ],
+    [ '-A -A',                  'a g h m n' ],
     [ '-T',                     'centos centos4 centos4-x86_64 centos5 centos5-i386 centos5-x86_64 laptop public vps' ],
     # Try unrecognised tags as hosts
-    [ 'nox',                    'centos centos5 centos5-i386 laptop' ],
-    [ 'axe nox',                'centos centos5 centos5-i386 centos5-x86_64 laptop public' ],
+    [ 'n',                      'centos centos5 centos5-i386 laptop' ],
+    [ 'a n',                    'centos centos5 centos5-i386 centos5-x86_64 laptop public' ],
     # Invalid data
     [ 'foo',                    %r{ \btag\b  .* not\sfound }x ],
     [ 'centos foo',             %r{ \btag\b  .* not\sfound }x ],
     [ '-t foo',                 %r{ \bhost\b .* not\sfound }x ],
-    [ '-t nox foo',             %r{ \bhost\b .* not\sfound }x ],
+    [ '-t n foo',               %r{ \bhost\b .* not\sfound }x ],
   ]
 
   def test_hosttag
+    test_args = '--server localhost --ns hosttag_testing'
+
+    # Setup
+    datadir = "#{File.dirname(__FILE__)}/test_data"
+    File.directory?(datadir) or throw "missing datadir #{datadir}"
+    `../bin/htimport #{test_args} --delete --datadir #{datadir}`
+
     TESTS.each do |args, expected|
-      got = `../bin/hosttag.rb #{args}`.chomp
+      got = `../bin/hosttag #{test_args} #{args}`.chomp
       if expected.is_a?(String)
         assert_equal(expected, got)
       elsif expected.is_a?(Regexp)
