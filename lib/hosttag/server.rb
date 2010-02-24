feae96a3289eabac04c4c203f163facdd292e1d9
Gavin Carr <gavin@openfusion.com.au>
2010-02-24 12:01:10 +0000

    Add some nice error handling for invalid server hostname (incl. default hosttag).

--- lib/hosttag/server.rb
+++ lib/hosttag/server.rb
@@ -2,12 +2,23 @@
 
 require 'rubygems'
 require 'redis'
+require 'resolv'
 
 module Hosttag
   class Server < Redis
     def initialize(options)
       @defaults = { :server => 'hosttag', :port => 6379, :namespace => 'hosttag' }
       @defaults.merge!(options)
+       
+      # Check :server name resolves
+      begin
+        Resolv.getaddress( @defaults[:server] )
+      rescue Resolv::ResolvError
+        raise Resolv::ResolvError, 
+          "Host '#{@defaults[:server]}' does not resolve (try --server <hostname>?)", caller
+      end
+
+      # Connect to redis
       super( :host => @defaults[:server], :port => @defaults[:port] )
     end
 
