0c0d5b30e13652537854cf68227572c6e7c2e9d8
Gavin Carr <gavin@openfusion.com.au>
2010-12-21 17:46:06 +0000

    Add test/test_htset_bin.rb unit tests.

---
+++ test/test_htset_bin.rb
@@ -0,0 +1,97 @@
+#!/usr/bin/ruby
+
+require 'test/unit'
+require 'ftools'
+
+require 'hosttag'
+include Hosttag
+
+class TestHtsetBin < Test::Unit::TestCase
+
+  TESTS = [
+    [ 
+      [ 'htset', 'foo', 'centos' ],    # operation
+      [ 'foo' ],                       # ht -A output
+      [ 'foo' ],                       # ht -A -A output
+      [ 'centos' ],                    # ht -T output
+    ],
+    [ 
+      [ 'htset', 'foo', 'centos5' ],
+      [ 'foo' ],
+      [ 'foo' ],
+      [ 'centos', 'centos5' ],
+    ],
+    [ 
+      [ 'htset', 'bar', 'centos' ],
+      [ 'bar', 'foo' ],
+      [ 'bar', 'foo' ],
+      [ 'centos', 'centos5' ],
+    ],
+    [ 
+      [ 'htset', 'bar', 'centos4' ],
+      [ 'bar', 'foo' ],
+      [ 'bar', 'foo' ],
+      [ 'centos', 'centos4', 'centos5' ],
+    ],
+    [ 
+      [ 'htset', 'bar', 'SKIP' ],
+      [ 'foo' ],
+      [ 'bar', 'foo' ],
+      [ 'centos', 'centos4', 'centos5' ],
+    ],
+    [ 
+      [ 'htdel', 'bar', 'SKIP' ],
+      [ 'bar', 'foo' ],
+      [ 'bar', 'foo' ],
+      [ 'centos', 'centos4', 'centos5' ],
+    ],
+    [ 
+      [ 'htdel', 'bar', 'centos4' ],
+      [ 'bar', 'foo' ],
+      [ 'bar', 'foo' ],
+      [ 'centos', 'centos5' ],
+    ],
+    [ 
+      [ 'htdel', 'bar', 'centos' ],
+      [ 'foo' ],
+      [ 'foo' ],
+      [ 'centos', 'centos5' ],
+    ],
+    [ 
+      [ 'htdel', 'foo', 'centos' ],
+      [ 'foo' ],
+      [ 'foo' ],
+      [ 'centos5' ],
+    ],
+    [ 
+      [ 'htdel', 'foo', 'centos5' ],
+      [],
+      [],
+      [],
+    ],
+  ]
+
+  def setup
+    @test_args = '--server localhost --ns hosttag_testing'
+    @test_opts = { :server => 'localhost', :namespace => 'hosttag_testing', :debug => false }
+    datadir = "#{File.dirname(__FILE__)}/data_reset"
+    @bindir = File.join(File.dirname(__FILE__), '..', 'bin')
+    File.directory?(datadir) or throw "missing datadir #{datadir}"
+    `#{@bindir}/htimport #{@test_args} --delete --datadir #{datadir}`
+  end
+
+  def test_htset
+    TESTS.each do |op, hosts1, hosts2, tags1|
+      cmd = op.join(' ')
+      bin = op.shift()
+      `#{@bindir}/#{bin} #{@test_args} #{op.join(' ')}`
+
+      # Check results
+      assert_equal(hosts1, hosttag_all_hosts(@test_opts), "all_hosts, #{cmd}")
+      assert_equal(hosts2, hosttag_all_hosts(@test_opts.merge({ :include_skip? => true })), 
+        "all_hosts include_skip, #{cmd}")
+      assert_equal(tags1, hosttag_all_tags(@test_opts),  "all_tags, #{cmd}")
+    end
+  end
+end
+
--- test/ts_all.rb
+++ test/ts_all.rb
@@ -1,3 +1,4 @@
 require 'test_hosttag.rb'
 require 'test_ht.rb'
 require 'test_htset.rb'
+require 'test_htset_bin.rb'
