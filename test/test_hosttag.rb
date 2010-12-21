fbc4fb0dc4f26d5704b57c21cfcff6de87879057
Gavin Carr <gavin@openfusion.com.au>
2010-12-21 14:48:05 +0000

    Add more unit tests for Hosttag module in test_hosttag.rb.

--- test/test_hosttag.rb
+++ test/test_hosttag.rb
@@ -9,7 +9,7 @@ include Hosttag
 class TestHosttag < Test::Unit::TestCase
   def setup
     @test_args = '--server localhost --ns hosttag_testing'
-    @test_opts = { :server => 'localhost', :namespace => 'hosttag_testing' }
+    @test_opts = { :server => 'localhost', :namespace => 'hosttag_testing', :debug => false }
     datadir = "#{File.dirname(__FILE__)}/data_hosttag"
     bindir = File.join(File.dirname(__FILE__), '..', 'bin')
     File.directory?(datadir) or throw "missing datadir #{datadir}"
@@ -17,41 +17,82 @@ class TestHosttag < Test::Unit::TestCase
   end
 
   # format: args (array), options (hash) => expected (array)
-  SET1 = [
+  TAGSET = [
     [ %w{centos},           {},                 %w{a m n} ],
     [ %w{laptop},           {},                 %w{n} ],
     [ %w{vps},              {},                 %w{m} ],
     [ %w{laptop vps},       {},                 %w{} ],
     [ %w{laptop vps},       { :rel => :and },   %w{} ],
     [ %w{laptop vps},       { :rel => :or },    %w{m n} ],
+  ]
+  HOSTSET = [
     [ %w{n},                {},                 %w{centos centos5 centos5-i386 laptop} ],
     [ %w{a},                {},                 %w{centos centos5 centos5-x86_64 public} ],
     [ %w{m},                {},                 %w{centos centos4 centos4-x86_64 public vps} ],
     [ %w{a n},              {},                 %w{centos centos5 centos5-i386 centos5-x86_64 laptop public} ],
-#   [ %w{-to a n},                %w{centos centos5 centos5-i386 centos5-x86_64 laptop public} ],
-#   [ %w{-t -o a n},              %w{centos centos5 centos5-i386 centos5-x86_64 laptop public} ],
-#   [ %w{-ta a n},                %w{centos centos5} ],
-#   [ %w{-t -a a n},              %w{centos centos5} ],
-#   [ %w{-t -a a n m},            %w{centos} ],
-    # All cases
-#   [ '-A',                     'a m n' ],
-#   [ '-A -A',                  'a g h m n' ],
-#   [ '-T',                     'centos centos4 centos4-x86_64 centos5 centos5-i386 centos5-x86_64 laptop public vps' ],
-    # Try unrecognised tags as hosts
-#   [ 'n',                      'centos centos5 centos5-i386 laptop' ],
-#   [ 'a n',                    'centos centos5 centos5-i386 centos5-x86_64 laptop public' ],
+    [ %w{a n},              { :rel => :or },    %w{centos centos5 centos5-i386 centos5-x86_64 laptop public} ],
+    [ %w{a n},              { :rel => :and },   %w{centos centos5} ],
+    [ %w{a n m},            { :rel => :and },   %w{centos} ],
+  ]
+
     # Invalid data
 #   [ 'foo',                    %r{ \btag\b  .* not\sfound }x ],
 #   [ 'centos foo',             %r{ \btag\b  .* not\sfound }x ],
 #   [ '-t foo',                 %r{ \bhost\b .* not\sfound }x ],
 #   [ '-t n foo',               %r{ \bhost\b .* not\sfound }x ],
-  ]
 
   def test_hosttag_lookup
-    SET1.each do |args, opts, expected|
+    TAGSET.each do |args, opts, expected|
       opts.merge!(@test_opts)
       got = hosttag_lookup(args, opts)
       assert_equal(expected, got, "args: #{args}")
+    end
+    HOSTSET.each do |args, opts, expected|
+      opts.merge!(@test_opts)
+      got = hosttag_lookup(args, opts)
+      assert_equal(expected, got, "args: #{args}")
+    end
+  end
+
+  def test_hosttag_lookup_tags
+    TAGSET.each do |args, opts, expected|
+      opts.merge!(@test_opts)
+      got = hosttag_lookup_tags(args, opts)
+      assert_equal(expected, got, "args: #{args}")
+    end
+  end
+
+  def test_hosttag_lookup_hosts
+    HOSTSET.each do |args, opts, expected|
+      opts.merge!(@test_opts)
+      got = hosttag_lookup_hosts(args, opts)
+      assert_equal(expected, got, "args: #{args}")
+    end
+  end
+
+  # format: options (hash) => expected (array)
+  TAGSET_ALL = [
+    [ {},                           %w{centos centos4 centos4-x86_64 centos5 centos5-i386 centos5-x86_64 laptop public vps} ],
+    [ { :include_skip? => true },   %w{SKIP centos centos4 centos4-x86_64 centos5 centos5-i386 centos5-x86_64 laptop public vps} ],
+  ]
+  def test_hosttag_all_tags
+    TAGSET_ALL.each do |opts, expected|
+      mopts = opts.merge(@test_opts)
+      got = hosttag_all_tags(mopts)
+      assert_equal(expected, got, "opts: #{opts}")
+    end
+  end
+
+  # format: options (hash) => expected (array)
+  HOSTSET_ALL = [
+    [ {},                           %w{a m n} ],
+    [ { :include_skip? => true },   %w{a g h m n} ],
+  ]
+  def test_hosttag_all_hosts
+    HOSTSET_ALL.each do |opts, expected|
+      mopts = opts.merge(@test_opts)
+      got = hosttag_all_hosts(mopts)
+      assert_equal(expected, got, "opts: #{opts}")
     end
   end
 
