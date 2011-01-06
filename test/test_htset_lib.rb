946a97acd9a3285597c707d9cfcfbbe557b60644
Gavin Carr <gavin@openfusion.com.au>
2011-01-06 15:23:22 +0000

    Add more skip test cases to test_htset_lib.rb, and ht -T -T output testing.

--- test/test_htset_lib.rb
+++ test/test_htset_lib.rb
@@ -14,7 +14,7 @@ class TestHtsetLib < Test::Unit::TestCase
     datadir = "#{File.dirname(__FILE__)}/data_reset"
     @bindir = File.join(File.dirname(__FILE__), '..', 'bin')
     File.directory?(datadir) or throw "missing datadir #{datadir}"
-    `#{@bindir}/htimport #{@test_args} --delete --datadir #{datadir}`
+    `#{@bindir}/htimport #{@test_args} --delete --yes --datadir #{datadir}`
   end
 
   # -----------------------------------------------------------------------
@@ -22,62 +22,86 @@ class TestHtsetLib < Test::Unit::TestCase
   # -----------------------------------------------------------------------
 
   TESTS = [
-    [ 
+    [
       %w{htset foo centos},         # operation
       %w{foo},                      # ht -A output
       %w{foo},                      # ht -A -A output
       %w{centos},                   # ht -T output
+      %w{centos},                   # ht -T -T output
     ],
-    [ 
+    [
       %w{htset foo centos5},
       %w{foo},
       %w{foo},
       %w{centos centos5},
+      %w{centos centos5},
     ],
-    [ 
+    [
       %w{htset bar centos},
       %w{bar foo},
       %w{bar foo},
       %w{centos centos5},
+      %w{centos centos5},
     ],
-    [ 
+    [
       %w{htset bar centos4},
       %w{bar foo},
       %w{bar foo},
       %w{centos centos4 centos5},
+      %w{centos centos4 centos5},
     ],
-    [ 
+    [
       %w{htset bar SKIP},
       %w{foo},
       %w{bar foo},
-      %w{centos centos4 centos5},
+      %w{centos centos5},
+      %w{SKIP centos centos4 centos5},
     ],
-    [ 
+    [
+      %w{htset bar centos4-i386},
+      %w{foo},
+      %w{bar foo},
+      %w{centos centos5},
+      %w{SKIP centos centos4 centos4-i386 centos5},
+    ],
+    [
+      %w{htdel bar centos4},
+      %w{foo},
+      %w{bar foo},
+      %w{centos centos5},
+      %w{SKIP centos centos4-i386 centos5},
+    ],
+    [
       %w{htdel bar SKIP},
       %w{bar foo},
       %w{bar foo},
-      %w{centos centos4 centos5},
+      %w{centos centos4-i386 centos5},
+      %w{centos centos4-i386 centos5},
     ],
-    [ 
-      %w{htdel bar centos4},
-      %w{bar foo},
-      %w{bar foo},
-      %w{centos centos5},
-    ],
-    [ 
-      %w{htdel bar centos},
-      %w{foo},
-      %w{foo},
-      %w{centos centos5},
-    ],
-    [ 
-      %w{htdel foo centos},
-      %w{foo},
-      %w{foo},
-      %w{centos5},
-    ],
-    [ 
+    [
       %w{htdel foo centos5},
+      %w{bar foo},
+      %w{bar foo},
+      %w{centos centos4-i386},
+      %w{centos centos4-i386},
+    ],
+    [
+      %w{htdel foo centos},
+      %w{bar},
+      %w{bar},
+      %w{centos centos4-i386},
+      %w{centos centos4-i386},
+    ],
+    [
+      %w{htdel bar centos},
+      %w{bar},
+      %w{bar},
+      %w{centos4-i386},
+      %w{centos4-i386},
+    ],
+    [
+      %w{htdel bar centos4-i386},
+      [],
       [],
       [],
       [],
@@ -85,7 +109,7 @@ class TestHtsetLib < Test::Unit::TestCase
   ]
 
   def test_hosttag_add_delete_tags
-    TESTS.each do |op, all_hosts, all_hosts2, all_tags|
+    TESTS.each do |op, all_hosts, all_hosts_full, all_tags, all_tags_full|
       cmd = op.join(' ')
       bin = op.shift()
       if bin == 'htset'
@@ -96,9 +120,11 @@ class TestHtsetLib < Test::Unit::TestCase
 
       # Check results
       assert_equal(all_hosts, hosttag_all_hosts(@test_opts), "all_hosts, #{cmd}")
-      assert_equal(all_hosts2, hosttag_all_hosts(@test_opts.merge({ :include_skip? => true })), 
-        "all_hosts include_skip, #{cmd}")
+      assert_equal(all_hosts_full, hosttag_all_hosts(@test_opts.merge({ :include_skip? => true })),
+        "all_hosts_full, #{cmd}")
       assert_equal(all_tags, hosttag_all_tags(@test_opts),  "all_tags, #{cmd}")
+      assert_equal(all_tags_full, hosttag_all_tags(@test_opts.merge({ :include_skip? => true })),
+        "all_tags_full, #{cmd}")
     end
   end
 
@@ -115,22 +141,22 @@ class TestHtsetLib < Test::Unit::TestCase
   ]
   TESTS2 = [
     # Format: hosts to delete, ht -A output, ht -T output
-    [ 
+    [
       %w{},
       %w{bar cat dog foo},
       %w{centos centos5 centos5-x86_64 debian debian4 dell fedora fedora14 hp supermicro ubuntu ubuntu-lucid},
     ],
-    [ 
+    [
       %w{foo},
       %w{bar cat dog},
       %w{debian debian4 dell fedora fedora14 hp supermicro ubuntu ubuntu-lucid},
     ],
-    [ 
+    [
       %w{cat dog},
       %w{bar},
       %w{hp ubuntu ubuntu-lucid},
     ],
-    [ 
+    [
       %w{bar},
       %w{},
       %w{},
